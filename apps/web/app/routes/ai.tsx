17bf459e801b40e368787954587df88e0fac6529
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2024-08-24 16:56:05 +0530

    done

--- apps/web/app/routes/ai.tsx
+++ apps/web/app/routes/ai.tsx
@@ -1,42 +1,65 @@
 import { ActionFunctionArgs, json } from '@remix-run/cloudflare'
-import { Form, useActionData } from '@remix-run/react'
+import { useState } from 'react'
 
 export const action = async ({ request, context }: ActionFunctionArgs) => {
   const formData = await request.formData()
   const prompt = formData.get('prompt') as string
-  const errors: { prompt: string | null } = { prompt: null }
+  const errors = {} as Record<string, string>
   if (!prompt) {
     errors.prompt = 'Prompt is required'
-    return json({ erro: errors }, { status: 400 })
+    return json({ error: errors }, { status: 400 })
   }
   if (prompt.length > 500) {
     errors.prompt = 'Prompt is too long'
     return json({ error: errors }, { status: 400 })
   }
-  console.log('prompt:', prompt)
   try {
-    console.log(context.cloudflare.env.AI)
     const response = await context.cloudflare.env.AI.run(
       '@cf/bytedance/stable-diffusion-xl-lightning',
-      {
-        prompt,
-        numResults: 1,
-      },
+      { prompt },
     )
-    return json({ response }, { status: 200 })
+    return new Response(response, {
+      status: 200,
+      headers: {
+        'Content-Type': 'img/png',
+      },
+    })
   } catch (error) {
     console.error('Error generating image:', error)
-    return json({ error: 'Failed to generate image' }, { status: 500 })
+    errors.gen = 'Failed to generate image'
+    return json({ error: errors }, { status: 500 })
   }
 }
 
 const Ai = () => {
-  const actionData = useActionData<typeof action>()
+  const [p, sp] = useState('')
+  const [i, si] = useState<string | null>(null)
+
+  const getImg = async () => {
+    const formData = new FormData()
+    formData.append('prompt', p)
+    const res = await fetch('/ai?_data=routes%2Fai', {
+      method: 'POST',
+      body: formData,
+    })
+    const img = await res.blob()
+    console.log(img)
+    console.log(URL.createObjectURL(img))
+    si(URL.createObjectURL(img))
+  }
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-center gap-8 bg-red-500">
       <p className="font-bold text-5xl">lmaooooooo img generation</p>
-      <Form method="post" className="flex flex-col gap-4">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          getImg()
+        }}
+        className="flex flex-col gap-4"
+      >
         <textarea
+          value={p}
+          onChange={(e) => sp(e.target.value)}
           className="min-w-64 min-h-32"
           name="prompt"
           placeholder="prompt"
@@ -44,12 +67,8 @@ const Ai = () => {
         <button className="px-4 py-2 bg-blue-500" type="submit">
           Generate
         </button>
-      </Form>
-      {actionData?.response ? (
-        <img src={actionData.response.generatedImageUrl} alt="generated img" />
-      ) : actionData?.error ? (
-        <p className="text-blue-500">{actionData.error.prompt}</p>
-      ) : null}
+      </form>
+      {i && <img className="w-[80vw]" src={i} alt="generated img" />}
     </div>
   )
 }
